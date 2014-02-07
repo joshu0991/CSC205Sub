ede634e6d400c8f30654cf6ff3a1dd50e93e55c4
josh <joshualilly91@gmail.com>
2014-02-07 18:33:28 -0500

    continued working on double p calculator need to convert fraction

--- IEEE-754_Converter/src/ieee/Conversion_Logic/DoubleP_Conversion_Logic.java
+++ IEEE-754_Converter/src/ieee/Conversion_Logic/DoubleP_Conversion_Logic.java
@@ -9,6 +9,7 @@ public class DoubleP_Conversion_Logic {
 		
 		if(sign == true){
 			binNum += '1';
+			
 		}else{
 			binNum += '0';
 		}
@@ -16,14 +17,57 @@ public class DoubleP_Conversion_Logic {
 		String[] splitNum = decNum.split("\\.");
 		int exponet = 0;
 		String wholeNum = splitNum[0];
+		wholeNum = wholeNum.substring(1);
 		String fraction = splitNum[1];
 		fraction = "0." + fraction;
-		int intWholeNum = l.convertToInt(wholeNum);
+		int intWholeNum = convertToInt(wholeNum);
 		String binWholeNum = Integer.toBinaryString(intWholeNum);
 		exponet = binWholeNum.length() - 1;
-		//int biasNum = "some 64 bit bias " + exponet;
+		int biasNum = 1023 + exponet;
+		String exp = convertBiasToBin(biasNum);
+		
+		String frontMantissa = binWholeNum.substring(1);
+
+		String binFractionNum = convertFraction(fraction, exponet);
 		
 		return binNum;
 	}
 
+	
+	
+	public String convertFraction(String frac, int exp){
+		String fracString = ""; // fraction for 64 bits = 64 - 12 = 52 bits
+		
+		
+		return fracString;
+	}
+	
+	
+	public String convertBiasToBin(int bias) {
+		String binExp = Integer.toBinaryString(bias);
+		String rString = "";
+		if (binExp.length() < 11) {
+			for (int counter = binExp.length(); counter < 11; counter++) {
+				rString += "0" + binExp;
+				binExp = rString;
+				rString = "";
+			}
+		}
+		return binExp;
+	}
+	
+	
+	public int convertToInt(String num) {
+		int intNum;
+		intNum = Integer.parseInt(num);
+		return intNum;
+	}
+	
+	public static void main(String[] args) {
+		String numbertoconvert = "-20.6252";
+		//String binNumToConv = "11000001101001010000000001101001";
+		DoubleP_Conversion_Logic l = new DoubleP_Conversion_Logic();
+		l.convertToDoubleP(numbertoconvert);
+		
+	}
 }
